750f5a1b9a627078f6948cda35e4b6e5b30c4304
TolotraSmile <tolotra.raharison@gmail.com>
2018-04-03 16:37:22 +0300

    first commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { searchText: '', placeholder: 'Tapez un film...' }
+  }
+
+  render () {
+    return (
+      <div>
+        <input value={this.state.searchText}
+               onChange={(e) => this.handleChange(e)}
+               placeholder={this.state.placeholder}/>
+        <p>{this.state.searchText}</p>
+      </div>
+    )
+  }
+
+  handleChange (event) {
+    this.setState({ searchText: event.target.value })
+    console.log(this.state.searchText)
+  }
+}
+
+export default SearchBar
